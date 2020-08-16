9eea1272f15949b6c01edc9175794a8db3d3f476
user <daiokaze@gmail.com>
2020-08-16 20:44:18 +0400

    поправка в dev

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -11,7 +11,7 @@ namespace ClassLibrary1
         public Class1(string p, string p1="тестовое сообщение")
         {
             Prop = p.ToUpper();
-            Prop1 = p1.ToLower();
+            Prop1 = p1;
         }
     }
 }    
