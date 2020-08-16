0bc0638c9ee455008e5ca6eae3aa637e0f4b6f31
user <daiokaze@gmail.com>
2020-08-16 20:27:18 +0400

    тримаю Prop в Dev ветке

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -10,7 +10,7 @@ namespace ClassLibrary1
 
         public Class1(string p, string p1="тестовое сообщение")
         {
-            Prop = p;
+            Prop = p.Trim();
             Prop1 = p1;
         }
     }
