bf63816541d15829dbf557fd877724cbcdfcae9b
user <daiokaze@gmail.com>
2020-08-16 20:13:19 +0400

    опять поправил

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -5,9 +5,17 @@ namespace ClassLibrary1
     public class Class1
     {
         public string Prop;
-        public Class1(string p)
+
+        public string Prop1
+        {
+            get => Prop;
+            set => Prop = value;
+        }
+
+        public Class1(string p, string p1)
         {
             Prop = p;
+            Prop1 = p1;
         }
     }
 }
